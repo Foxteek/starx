62198fcc167493ee9ba349a25e244a7740111d16
Foxtea <iiijjjuhhk@163.com>
2023-03-10 21:43:13 +0800

    Update xiarou-set1.js

--- js/xiarou-set1.js
+++ js/xiarou-set1.js
@@ -16,17 +16,13 @@ github：https://github.com/imsyy/sou2
 由 夏柔 二次修改
 github：https://www.aa1.cn
 日期：2022-12-18
-*/
 ========================================
-
 由 Foxtea 二次修改
-
 github：https://www.github.com/Foxteek/starx
-
-日期：2022-12-18
-
+日期：2023-2-28
 */
 ========================================
+
 // 默认搜索引擎列表
 var se_list_preinstall = {
     '1': {
